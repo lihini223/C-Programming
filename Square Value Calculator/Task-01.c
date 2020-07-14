c334c64341c072cf12dbda8feaf962413b27d52c
Lihini Nisansala <lihininisansala95@gmail.com>
2020-07-14 23:13:35 +0530

    update task 1

--- Square Value Calculator/Task-01.c
+++ Square Value Calculator/Task-01.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+int square_value,num; //declare variables
+
+printf("\n\nWelcome to Square Value Calculator !\n\n");
+
+//getting inputs to calculate square value
+printf("Enter number to calculate Square Value:");
+scanf("%d",&num);
+
+//calculating square value
+square_value = num * num ;
+
+//print Square value along with the user input number
+printf("The square value of %d is : %d",num,square_value);
+
+//print current system time
+time_t rawtime;
+struct tm * timeinfo;
+time(&rawtime);
+timeinfo = localtime(&rawtime);
+printf("\n\nCurrent system time and date: %s", asctime(timeinfo));
+
+return 0;
+}
 
