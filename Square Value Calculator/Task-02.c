dc0f44dbaf623f1f09725b40b7ee9a2469491659
Lihini Nisansala <lihininisansala95@gmail.com>
2020-07-18 03:37:50 +0530

    update task2

--- Square Value Calculator/Task-02.c
+++ Square Value Calculator/Task-02.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+int square_value=1,num,count; //declare variables
+
+printf("\nWelcome to Square Value Calculator !\n\n");
+
+
+
+
+return 0;
+}
 
